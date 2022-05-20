62ddeb57f9ed81f75f43bf3f1a4935e829739009
Arley Rivas G <arivas@innside.co>
2022-05-20 00:01:24 -0500

    Pipes date + cambio de idioma  a español colombia a toda la App

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,33 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
 // Módulo personalizado
-import { SharedModule } from './shared/shared.module';
 import { AppRouterModule } from './app-router.module';
+import { SharedModule } from './shared/shared.module';
+import { VentasModule } from './ventas/ventas.module';
 
+
+// Cambiar el locale de la APP
+import localeEspColombia from '@angular/common/locales/es-CO';
+import {registerLocaleData} from '@angular/common';
+registerLocaleData(localeEspColombia); //registramos el español colombiano en nuestra App
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [ //Organizamos por prioridad, primero los de angular
-    BrowserModule,
     AppRouterModule,
+    BrowserModule,
     SharedModule,
+    VentasModule
 
   ],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'es-CO'}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
