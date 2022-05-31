26198819d27a0367167f82e2a34d9731e572c0e5
Arley Rivas G <arivas@innside.co>
2022-05-31 00:56:13 -0500

    i18nPlural mensaje de acuerdo al número de clientes

--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -8,13 +8,34 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class NoComunesComponent {
- 
+
+  //i18nSelect
   nombre: string = 'Arley';
   genero: string = 'masculino';
 
   invitacionMapping = {
-    'masculino' : 'invitarlo',
-    'femenino' : 'invitarla'
-   }
+    'masculino': 'invitarlo',
+    'femenino': 'invitarla',
+    'other': 'invitarte'
+  }
+
+  //i18nPlural
+  clientes: any = ['Arley', 'Kelly', 'Juan Carlos'];
+  clientesMapa = {
+    '=0': 'no tenemos ningún cliente esperando.',
+    '=1': 'tenemos un cliente esperando.',
+    // el # representa el valor que estamos recibiendo, seria igual a tener  'other': `tenemos ${this.clientes.length} clientes esperando.`
+    'other': 'tenemos # clientes esperando.'
+  }
+
+
+  cambiarNombre() {
+    this.nombre = 'Johanna';
+    this.genero = 'femenino'
+  }
+
+  borrarCliente() {
+    this.clientes.pop();
+  }
 
 }
